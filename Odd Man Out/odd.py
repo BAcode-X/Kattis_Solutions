0fda9ab67e09aa49d1b477ff8515156d19cf41f7
BAcode-X <56090746+BAcode-X@users.noreply.github.com>
2019-12-19 20:22:48 +0300

    Create odd.py

---
+++ Odd Man Out/odd.py
@@ -0,0 +1,12 @@
+a=int(input())
+for i in range(a):
+    b=int(input())
+    c=[int(_)for _ in input().split()]
+    e=[]
+    for j in c:
+        if j in e:
+            e.remove(j)
+        else:
+            e.append(j)
+    print(f"Case #{i+1}: {e[0]}")
+
