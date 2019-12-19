dc3a23bd73a5fc663c7c6d597cbadb7a0ffaac0f
BAcode-X <56090746+BAcode-X@users.noreply.github.com>
2019-12-19 19:46:12 +0300

    Create sibice.py

---
+++ Sibice/sibice.py
@@ -0,0 +1,9 @@
+import math
+a=input()
+b=a.split()
+for i in range(int(b[0])):
+    c=int(input())
+    if c<=int(b[1]) or c<=int(b[2]) or c<=int(math.sqrt(math.pow(int(b[1]),2)+math.pow(int(b[2]),2))):
+        print ("DA")
+    else:
+        print ("NE")
