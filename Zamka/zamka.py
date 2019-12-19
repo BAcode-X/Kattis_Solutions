2ccb1064c668a885e33f0f60f944c3646ee87261
BAcode-X <56090746+BAcode-X@users.noreply.github.com>
2019-12-19 20:11:00 +0300

    Create zamka.py

---
+++ Zamka/zamka.py
@@ -0,0 +1,11 @@
+l=int(input())
+d=int(input())
+x=int(input())
+a=[]
+for i in range(l,d+1):
+    k=sum([int(j) for j in str(i)])
+    if k == x:
+        a.append(i)
+print(min(a))
+prin
+t(max(a))
