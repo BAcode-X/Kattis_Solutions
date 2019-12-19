e9ea07830883029db6e11a2166a6a45d34910d24
BAcode-X <56090746+BAcode-X@users.noreply.github.com>
2019-12-19 20:28:33 +0300

    Create Icpc.py

---
+++ ICPC Awards/Icpc.py
@@ -0,0 +1,16 @@
+a=int(input())
+uni=[]
+team=[]
+for i in range(a):
+    b=input().split()
+    c=b[0]
+    d=b[1]
+    if c not in uni:
+        uni.append(c)
+        team.append(d)
+
+for i in range(len(uni)):
+    if i>11:
+        break
+    else:
+        print(uni[i],team[i])
