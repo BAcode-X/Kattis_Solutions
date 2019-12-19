a0670c6459d5a6761460655efe891ee725463d1e
BAcode-X <56090746+BAcode-X@users.noreply.github.com>
2019-12-19 20:27:43 +0300

    Create kemi.py

---
+++ Kemiji/kemi.py
@@ -0,0 +1,15 @@
+a=list(input())
+vow=["a","e","i","o","u"]
+b=[]
+f=0
+for i in range(len(a)):
+    if f:
+        f-=1
+        continue
+    if a[i] not in vow:
+        b.append(a[i])
+    else:
+        b.append(a[i])
+        f=2
+print(''.join(b))
+
