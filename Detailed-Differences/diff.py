fe6f7d1fc171bdc8156ffceb3c254cf35e24d0a1
BAcode-X <56090746+BAcode-X@users.noreply.github.com>
2019-12-19 20:02:49 +0300

    Create diff.py

---
+++ Detailed-Differences/diff.py
@@ -0,0 +1,16 @@
+a=int(input())
+for i in range(a):
+    b=input()
+    c=input()
+    d=list(b)
+    e=list(c)
+    f=[]
+    for j in range(len(d)):
+        if d[j]==e[j]:
+            f.append(".")
+        else:
+            f.append("*")
+    g=''.join(f)
+    print(b)
+    print(c)
+    print(g)
