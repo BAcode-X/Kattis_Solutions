bf4a2586d1f42a11eb37e77af46d0d860981f6fa
BAcode-X <56090746+BAcode-X@users.noreply.github.com>
2019-12-19 19:53:23 +0300

    Create judge.py

---
+++ Judging-Moose/judge.py
@@ -0,0 +1,13 @@
+a=input()
+b=a.split()
+c=int(b[0])
+d=int(b[1])
+if c==0 and d==0:
+    print("Not a moose")
+else:
+    if c==d:
+        print("Even",d*2)
+    elif c>d:
+        print("Odd",c*2)
+    else:
+        print("Odd",d*2)
