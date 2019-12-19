f5a9af48082c2e43d4734b3d2c9c55406897c73d
BAcode-X <56090746+BAcode-X@users.noreply.github.com>
2019-12-19 20:31:51 +0300

    Create enco.py

---
+++ Encoded Message/enco.py
@@ -0,0 +1,16 @@
+import math
+x=int(input())
+for i in range(x):
+    a=input()
+    b=list(a)
+    c=int(math.sqrt(len(b)))
+    r=c
+    d=[]
+    for i in range(1,r+1,+1):
+        for j in range(1,r+1,+1):
+            d.append(b[c-i])
+            c=c+r
+        c=r 
+    print(''.join(d))
+    
+
